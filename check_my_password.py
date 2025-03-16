32097389fc2dc6e2539f8afb41a28370e5e21d27
lesliemw <l.marie1598@gmail.com>
2025-03-16 15:48:54 +0000

    update file

--- check_my_password.py
+++ check_my_password.py
@@ -1,6 +1,26 @@
 import requests
+import hashlib
 
-url = "https://api.pwnedpasswords.com/range/" + \
-    "70B64"
-response = requests.get(url)
-print(response.text)
+
+def request_api_data(query_char):
+    '''fetched data from the API using the first 5 characters of the hashed password'''
+    url = "https://api.pwnedpasswords.com/range/" + query_char
+    response = requests.get(url, timeout=10)
+    if response.status_code != 200:
+        raise RuntimeError(
+            f"Error fetching: {response.status_code}, check the API and try again")
+    return response
+
+
+def read_res(response):
+    '''Read the response from the API'''
+    print(response.text)
+
+
+def pwned_api_check(password):
+    '''Check if the password exists in the API response'''
+    sha1password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    print(first5_char, tail)
+    return response
