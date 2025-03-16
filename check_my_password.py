0c34fa476ffa6ace07578f5351923cbfce3b2af7
lesliemw <l.marie1598@gmail.com>
2025-03-16 15:56:50 +0000

    update file

--- check_my_password.py
+++ check_my_password.py
@@ -12,9 +12,13 @@ def request_api_data(query_char):
     return response
 
 
-def read_res(response):
-    '''Read the response from the API'''
-    print(response.text)
+def get_password_leaks_count(hashes, hash_to_check):
+    '''Read the number of times the password has been leaked'''
+    hashes = (line.split(":") for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if h == hash_to_check:
+            return count
+    return 0
 
 
 def pwned_api_check(password):
@@ -22,5 +26,15 @@ def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)
-    print(first5_char, tail)
-    return response
+    return get_password_leaks_count(response, tail)
+
+
+def main(password):
+    '''Main function to check the password using data collected from the API'''
+    count = pwned_api_check(password)
+    if count:
+        print(
+            f"{password} was found {count} times... you should probably change your password!")
+    else:
+        print(f"{password} was NOT found. Carry on!")
+    return "done!"
